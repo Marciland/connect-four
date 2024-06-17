c02258928ede3d7fdf9260c92c44ca10dd25da63
Marcel Witoschek <marcel.witoschek@gdata.de>
2024-06-17 18:54:35 +0200

    settings menu

--- components/menu.py
+++ components/menu.py
@@ -69,11 +69,53 @@ class SettingsMenu(MenuFrame):  # pylint: disable=too-many-ancestors
     def __init__(self, window) -> None:
         super().__init__(window=window)
         self.window = window
-        # cwd = getcwd()
+        cwd = getcwd()
         self.images.update({
-
+            'easy': PhotoImage(file=path.join(cwd, 'res/button/easy.png')),
+            'medium': PhotoImage(file=path.join(cwd, 'res/button/medium.png')),
+            'hard': PhotoImage(file=path.join(cwd, 'res/button/hard.png')),
+            'extreme': PhotoImage(file=path.join(cwd, 'res/button/extreme.png')),
+            'back': PhotoImage(file=path.join(cwd, 'res/button/back.png'))
         })
+        self.difficulty_buttons: dict[str, Button] = {}
         self._prepare_settings_menu()
+        self._change_difficulty(0, 'easy')
 
     def _prepare_settings_menu(self) -> None:
-        pass
+        easy = Button(master=self,
+                      image=self.images['easy'],
+                      command=lambda button='easy': self._change_difficulty(0, button))
+        easy.place(x=150, y=50, width=200, height=100)
+        easy.lift(self.background)
+        self.difficulty_buttons.update({'easy': easy})
+        medium = Button(master=self,
+                        image=self.images['medium'],
+                        command=lambda button='medium': self._change_difficulty(1, button))
+        medium.place(x=350, y=50, width=200, height=100)
+        medium.lift(self.background)
+        self.difficulty_buttons.update({'medium': medium})
+        hard = Button(master=self,
+                      image=self.images['hard'],
+                      command=lambda button='hard': self._change_difficulty(2, button))
+        hard.place(x=150, y=200, width=200, height=100)
+        hard.lift(self.background)
+        self.difficulty_buttons.update({'hard': hard})
+        extreme = Button(master=self,
+                         image=self.images['extreme'],
+                         command=lambda button='extreme': self._change_difficulty(3, button))
+        extreme.place(x=350, y=200, width=200, height=100)
+        extreme.lift(self.background)
+        self.difficulty_buttons.update({'extreme': extreme})
+        back = Button(master=self,
+                      image=self.images['back'],
+                      command=self.window.show_main_menu)
+        back.place(x=150, y=550, width=400, height=100)
+        back.lift(self.background)
+
+    def _change_difficulty(self, dif_value: int, difficulty: str) -> None:
+        self.window.set_difficulty(dif_value)
+        for _difficulty, button in self.difficulty_buttons.items():
+            if _difficulty == difficulty:
+                button.configure(state='disabled')
+            else:
+                button.configure(state='active')
